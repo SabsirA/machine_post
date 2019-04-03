e49469713cca0cebaf0988d267e82adec55c9c9b
Yellow <vladtagiev@bk.ru>
2019-04-03 23:28:54 +0300

    ugguh

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
-var lenta = [];
-var ukazka = 0;
-var timer;
-function render_lenta(a, b) {
+﻿var lenta = [];//массив в котором хранятся данные ленты
+var ukazka = 0;// индекс ячейки на котором находится указатель
+var size=39;// кол-во ячеек в ленте
+var Timer;//Переменная для остановки программы
+function render_lenta(a, b) {//функция которая рисует заданный участок ленты
     let html = ``;
     for (let i = a; i <= b; i++){
         html +=`<div class="cel ${lenta[i].mark ? 'mark':''} ${ i == ukazka ? 'active':''}" data-id="${i}"></div>`
     }
     $('.tape').html(html);
 }
-function init() {
+function init() {// функция которая обнуляет ленту
     lenta = [];
-    for (let i = 0; i < 20; i++){
+    for (let i = 0; i <= size; i++){
         lenta.push ({
             mark: false}
         )
     }
 }
-function add_command(){
+function add_command(){//функция которая добавляет новую команду 
     let html = ``;
     html +=`<div class="item">
         <div class="number"></div>
@@ -27,53 +28,57 @@ function add_command(){
             <option value="?">?</option>
             <option value="X">X</option>
             <option value="!">!</option></select></div>
-        <div class="number_command" contenteditable="true"></div>
+       <input type="number" class="number_command" min="1"></input>
         <div class="comment" contenteditable="true"></div>
     </div>`;
    $('.commands').append(html);
 }
-function execute(n) {
-    let command=$(`.item:eq(${n})`);
-    if(!command){
+function execute(n) {//функция которая выполняет команду и запускает на выполнение следущую команду
+    let command=$(`.item:eq(${n})`);//получаем команду под номером N
+    if(!command){//проверяется существование команды 
         return {
             'status':false,
             'message':`Ошибка. Команда ${n} не найдена`
         }
     }
     $('.run').removeClass ('run');
-    command.addClass('run');
-    let make = command.find('select').val();
-    let num=command.find('.number_command').html();
+    command.addClass('run');//подсвечиваем команду которую выполняем
+    let make = command.find('select').val();//получение значения команды
+    let num=command.find('.number_command').val();//получаем номер следующей команды
     switch (make) {
-        case '<': {ukazka--;
-            render_lenta(0, 19);
+        case '<': {//если команда-шаг влево
+		if (ukazka>0)
+		ukazka--;
+            render_lenta(0, size);
         break;};
-        case '>': {ukazka++;
-            render_lenta(0, 19);
+        case '>': {//если команда -шаг вправо
+		if (ukazka<size)
+		ukazka++;
+            render_lenta(0, size);
         break;};
-        case'!':return true;
-        case'V':
-            if(lenta[ukazka].mark){
+        case'!':return true;//если команда -остановка
+        case'V'://если команда -установить метку
+            if(lenta[ukazka].mark){// проверка существования метки
                 return {
                     'status':false,
                     'message':`Ошибка в команде ${n}. Указатель уже стоит`
                 }
             }
             lenta[ukazka].mark=true;
-        render_lenta(0,19);
+        render_lenta(0,size);
         break;
-        case 'X':
-            if(!lenta[ukazka].mark){
+        case 'X'://если команда- удалить метку
+            if(!lenta[ukazka].mark){//проверка существования метки
             return {
                 'status':false,
                 'message':`Ошибка в команде ${n}. Указателя нет`
             }
         }
             lenta[ukazka].mark=false;
-            render_lenta(0,19);
+            render_lenta(0,size);
             break;
-        case '?':
-            let  wars=command.find('.number_command').html().split(',');
+        case '?'://если команда-условие
+            let  wars=command.find('.number_command').val().toString().split('.');
             if (lenta[ukazka].mark){
                 num=wars[1];
             }else {
@@ -86,35 +91,35 @@ function execute(n) {
             'message':`Ошибка в команде ${n}. Отсутствует ссылка на следущую команду`
         }
     }
-    setTimeout(execute, $('.speed').val(),num);
+
+   Timer= setTimeout(execute, $('.speed').val(),num);//Отсроченный запуск следующей команды
 }
 
-$(function () {
+$(function () {//jQvery функция выполняющаяся после полной загрузки страницы
 
-   init();
-   ukazka = lenta.length / 2;
-   render_lenta(0, lenta.length - 1);
-   $('.tape').on('click', '.cel', function (e) {
+   init();//опустошает ленту
+   ukazka = lenta.length / 2;//ставим указку в середину ленты
+   render_lenta(0, lenta.length - 1);//рисует ленту
+   $('.tape').on('click', '.cel', function (e) {//вешаем событие на клик по ячейке ленты для установки/удаления метки
        let id= $(e.currentTarget).data('id');
    lenta[id].mark=!lenta[id].mark;
-   render_lenta(0, 19);
+   render_lenta(0, size);
    });
-    $('.button.left').on('click', function (e) {
+    $('.button.left').on('click', function (e) {//вешаем событие на клик по кнопке для смещения указателя влево
+        if (ukazka>0)
         ukazka--;
-        render_lenta(0, 19);
+        render_lenta(0, size);
     });
-    $('.button.right').on('click', function (e) {
+    $('.button.right').on('click', function (e) {//вешаем событие на клик по кнопке для смещения указателя вправо
+        if (ukazka<size)
         ukazka++;
-        render_lenta(0, 19);
+        render_lenta(0, size);
     });
-    $('.commands').on('click', '.item:last', function(e){add_command()});
-    $('.button.start').on('click', function (e) {
+    $('.commands').on('click', '.item:last', function(e){add_command()});//вешаем событие на клик по последней команде для добавление новой
+    $('.button.start').on('click', function (e) {//событие клика по кнопке старт
         (execute(1));
     });
-    $('.button.save').on('click', function (e) {
-       $.cookie('save',$('.command').html());
+    $('.button.stop').on('click', function (e) {//событие клика по кнопке старт
+        clearInterval(Timer);
     });
-    $('.button.load').on('click', function (e) {
-        $('.command').html(  $.cookie('save'));
-    })
 });
